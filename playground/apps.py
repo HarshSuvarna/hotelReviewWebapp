42b4815fc557204209fd1c422663397a6bfbd72d
Harsh Suvarna <harsh.suvarna0@gmail.com>
2024-02-09 15:24:07 +0000

    playgorund code

--- playground/apps.py
+++ playground/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class HotelreviewappConfig(AppConfig):
+class PlaygroundConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'HotelReviewApp'
+    name = 'playground'
