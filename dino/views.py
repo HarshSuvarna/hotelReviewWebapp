f79e9047dfdf281eb290038bbf75948dafd7c4ae
Harsh Suvarna <harsh.suvarna0@gmail.com>
2024-02-09 15:57:01 +0000

    index.html added

--- dino/views.py
+++ dino/views.py
@@ -5,7 +5,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, "index.html", {"message": "Welcome to Dino!"})
-    # return HttpResponse("Heath Check: OK")
 
 
 def healthCheck(request):
